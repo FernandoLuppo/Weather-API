e402c0d56534d1020ac4928e1f9d76c3915167cd
fernando <fernandoluppo2@gmail.com>
2023-10-29 18:50:53 -0300

    feat: create login method and adjust others methods related with password

--- src/services/User/UserService.ts
+++ src/services/User/UserService.ts
@@ -6,8 +6,12 @@ import {
   registerAuthenticateSchema
 } from "../../middleware"
 import { User } from "../../database/models/User"
+import { Token } from "../../database/models/Token"
 import { v4 as uuidv4 } from "uuid"
 import { handleCatchErrors } from "../../utils"
+import type { TokenService } from "../Token/TokenService"
+import dayjs from "dayjs"
+import { EncryptPasswordService } from "../EncryptPassword/EncryptPasswordService"
 
 export class UserService {
   public constructor(private readonly _req: Request) {}
@@ -30,8 +34,17 @@ export class UserService {
       return result
     }
 
+    const encryptPassword = new EncryptPasswordService(password)
+    const newPassword = encryptPassword.encrypt()
+
+    if (newPassword.isError) {
+      result.error = newPassword.error
+      result.isError = newPassword.isError
+      return result
+    }
+
     try {
-      await User.create({ id, name, email, password })
+      await User.create({ id, name, email, password: newPassword.content })
       result.content = { message: "User create with success!" }
 
       return result
@@ -40,7 +53,37 @@ export class UserService {
     }
   }
 
-  public login(): void {}
+  public async login(tokenService: TokenService): Promise<IResult> {
+    const result: IResult = { error: [""], isError: false, content: {} }
+    const { error, isError, content } = await this._loginValidation()
+
+    if (isError) {
+      result.isError = isError
+      result.error = error
+      return result
+    }
+
+    try {
+      const accessToken = tokenService.createToken(content.id, "60m")
+      const refreshToken = tokenService.createToken(content.id, "3d")
+      const refreshTokenExpiresDate = dayjs().add(3, "days").toDate()
+
+      await Token.create({
+        userToken: content.id,
+        token: refreshToken,
+        expireDat: refreshTokenExpiresDate
+      })
+
+      result.content = {
+        accessToken,
+        refreshToken,
+        message: "User logged with success!"
+      }
+      return result
+    } catch (error) {
+      return handleCatchErrors(error)
+    }
+  }
 
   private async _registerValidation(): Promise<IResult> {
     const result: IResult = { error: [""], isError: false, content: "" }
@@ -67,12 +110,29 @@ export class UserService {
 
   private async _loginValidation(): Promise<IResult> {
     const result: IResult = { error: [""], isError: false, content: "" }
+    const { email, password } = this._req.body
 
     try {
       await loginAuthenticateSchema.validate(this._req.body, {
         abortEarly: false
       })
 
+      const user = await User.findOne({ where: { email } })
+
+      if (user === null || user === undefined) {
+        return result
+      }
+
+      const encryptPassword = new EncryptPasswordService(password)
+      const { error, isError } = await encryptPassword.compare(user.password)
+
+      if (isError) {
+        result.isError = isError
+        result.error = error
+        return result
+      }
+
+      result.content = { id: user.id }
       return result
     } catch (err) {
       const errors: string[] = []
