e05dc79ea2b2219f291daefff51403086405d536
fernando <fernandoluppo2@gmail.com>
2023-11-12 17:38:27 -0300

    feat: create a new middleware to validate update infos route

--- src/middleware/authenticate/UserAuthenticate.ts
+++ src/middleware/authenticate/UserAuthenticate.ts
@@ -1,6 +1,7 @@
 import {
   loginAuthenticateSchema,
-  registerAuthenticateSchema
+  registerAuthenticateSchema,
+  updateInfosAuthenticateSchema
 } from "./authenticateSchema"
 import type { NextFunction, Request, Response } from "express"
 import type * as yup from "yup"
@@ -41,6 +42,32 @@ export class UserAuthenticate {
 
     try {
       await loginAuthenticateSchema.validate(req.body, {
+        abortEarly: false
+      })
+
+      next()
+    } catch (err) {
+      const errors: string[] = []
+
+      ;(err as yup.ValidationError).errors.forEach(error => {
+        errors.push(error)
+      })
+
+      result.isError = true
+      result.error = errors
+      res.status(401).send(result)
+    }
+  }
+
+  public async updateInfos(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const result = { error: [""], isError: false, content: "" }
+
+    try {
+      await updateInfosAuthenticateSchema.validate(req.body, {
         abortEarly: false
       })
 
