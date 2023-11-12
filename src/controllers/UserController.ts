571cb71bc21eafd1174a4ee936f328347aaa5878
fernando <fernandoluppo2@gmail.com>
2023-11-12 15:08:49 -0300

    fix: adjusted code response

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import type { Response } from "express"
 import type { UserService } from "../services/user/UserService"
 import type { IResult } from "../types"
+import type { TokenService } from "../services/token/TokenService"
 
 export class UserController {
   constructor(
@@ -10,14 +11,45 @@ export class UserController {
 
   public async createUser(): Promise<Response<any, Record<string, any>>> {
     const result: IResult = { error: [""], isError: false, content: {} }
-    const { error, isError } = await this._userService.register()
+    const { content, error, isError } = await this._userService.register()
 
     if (isError) {
       result.isError = true
       result.error = error
-      return this._res.status(401).send({ content: "Error when create user." })
+      return this._res.status(401).send(result)
     }
+    result.content = content
 
-    return this._res.status(201).send({ content: "User create with success!" })
+    return this._res.status(201).send(result)
+  }
+
+  public async getUser(
+    tokenService: TokenService
+  ): Promise<Response<any, Record<string, any>>> {
+    const result: IResult = { error: [""], isError: false, content: {} }
+
+    const { content, error, isError } =
+      await this._userService.login(tokenService)
+
+    if (isError) {
+      result.error = error
+      result.isError = isError
+      return this._res.status(401).send(result)
+    }
+    result.content = "User Logged with success"
+
+    return this._res
+      .status(200)
+      .cookie("accessToken", content.accessToken, {
+        maxAge: 3.6e6,
+        httpOnly: false,
+        sameSite: "lax"
+      })
+      .cookie("refreshToken", content.refreshToken, {
+        maxAge: 2.592e8,
+        httpOnly: false,
+        sameSite: "lax"
+      })
+      .send(result)
   }
 }
