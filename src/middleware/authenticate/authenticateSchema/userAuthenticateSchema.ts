36cdddc9bcbcc4595178ad707274e197ec8860cf
fernando <fernandoluppo2@gmail.com>
2023-11-12 17:40:01 -0300

    feat: created new validation schemas

--- src/middleware/authenticate/authenticateSchema/userAuthenticateSchema.ts
+++ src/middleware/authenticate/authenticateSchema/userAuthenticateSchema.ts
@@ -20,8 +20,6 @@ export const registerAuthenticateSchema = yup.object().shape({
     )
 })
 
-// [!@#\$%\^&\*]
-
 export const loginAuthenticateSchema = yup.object().shape({
   email: yup
     .string()
@@ -29,6 +27,38 @@ export const loginAuthenticateSchema = yup.object().shape({
     .trim()
     .email("Email field must contain a valid email."),
   password: yup
+    .string()
+    .required("Password field is required")
+    .trim()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    )
+})
+
+export const updateInfosAuthenticateSchema = yup.object().shape({
+  name: yup.string().min(3, "Name field needs more than 3 characters."),
+  email: yup.string().trim().email("Email field must contain a valid email."),
+  profileImage: yup
+    .string()
+    .required("Password field is required")
+    .trim()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    )
+})
+
+export const recoverPasswordAuthenticateSchema = yup.object().shape({
+  password: yup
+    .string()
+    .required("Password field is required")
+    .trim()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+  confirmPassword: yup
     .string()
     .required("Password field is required")
     .trim()
