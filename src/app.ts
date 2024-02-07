a5f38cfbfbb62fa60d906f4f79aff25376ad6f25
fernando <fernandoluppo2@gmail.com>
2024-02-07 19:28:38 -0300

    fix: adjusted cors and route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import {
-  userRouter,
-  tokenRouter,
-  recoverPasswordRouter,
-  weatherRouter
-} from "./routes"
+import { userRouter, recoverPasswordRouter, weatherRouter } from "./routes"
 
 import * as dotenv from "dotenv"
 dotenv.config()
@@ -14,11 +9,16 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
-app.use(cors({ credentials: true, origin: true }))
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+)
+
 app.use(cookieParser())
 
 app.use("/user", userRouter)
-app.use("/token", tokenRouter)
 app.use("/recover-password", recoverPasswordRouter)
 app.use("/weather", weatherRouter)
 
